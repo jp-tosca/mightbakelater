d596bb3c9d85384bb37bc20964f104920838bd15
Juan Pablo Tosca Villanueva <jptosca@gmail.com>
2025-07-22 01:13:00 -0400

    fix: update site URL and base path in configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,8 +12,8 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://jp-tosca.github.io",
-  base: "mightbakelater",
+  site: "https://janedoe.com",
+  base: "/",
   trailingSlash: "ignore",
   prefetch: {
     prefetchAll: true
